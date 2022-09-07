ae9eaf2fd4556f599643e796525fb53df67d4a88
Timo Peter <timoaccs@gmail.com>
2022-09-07 08:35:21 +0000

    FRONTEND: Visibility settings design

--- frontend/left_sidebar/visibility_settings/visibility_settings_layout.py
+++ frontend/left_sidebar/visibility_settings/visibility_settings_layout.py
@@ -16,50 +16,50 @@ def get_layout():
         [
             dbc.Checklist(
                 options=[
-                    {"label": "Show assets", "value": NodeTypes.ASSET.value},
+                    {"label": "Assets", "value": NodeTypes.ASSET.value},
                     {
-                        "label": "Show timeseries inputs",
+                        "label": "Timeseries inputs",
                         "value": NodeTypes.TIMESERIES_INPUT.value,
                     },
                     {
-                        "label": "Show supplementary files",
+                        "label": "Supplementary files",
                         "value": NodeTypes.SUPPLEMENTARY_FILE.value,
                     },
                     {
-                        "label": "Show database connections",
+                        "label": "Database connections",
                         "value": NodeTypes.DATABASE_CONNECTION.value,
                     },
                     {
-                        "label": "Show runtime connections",
+                        "label": "Runtime connections",
                         "value": NodeTypes.RUNTIME_CONNECTION.value,
                     },
-                    {"label": "Show units", "value": NodeTypes.UNIT.value},
+                    {"label": "Units", "value": NodeTypes.UNIT.value},
                     {
-                        "label": "Show timeseries clusters",
+                        "label": "Timeseries clusters",
                         "value": NodeTypes.TIMESERIES_CLUSTER.value,
                     },
                     {
-                        "label": "Show extracted keywords",
+                        "label": "Extracted keywords",
                         "value": NodeTypes.EXTRACTED_KEYWORD.value,
                     },
                     {
-                        "label": "Show asset similarities",
+                        "label": "Asset similarities",
                         "value": PseudoNodeTypes.ASSET_SIMILARITY_PSEUDO_NODE.value,
                     },
                     {
-                        "label": "Show annotation definitions",
+                        "label": "Annotation definitions",
                         "value": NodeTypes.ANNOTATION_DEFINITION.value,
                     },
                     {
-                        "label": "Show annotation instances",
+                        "label": "Annotation instances",
                         "value": NodeTypes.ANNOTATION_INSTANCE.value,
                     },
                     {
-                        "label": "Show annotation pre-indicators",
+                        "label": "Annotation pre-indicators",
                         "value": NodeTypes.ANNOTATION_PRE_INDICATOR.value,
                     },
                     {
-                        "label": "Show annotation timeseries matchers",
+                        "label": "Annotation timeseries matchers",
                         "value": NodeTypes.ANNOTATION_TS_MATCHER.value,
                     },
                 ],
